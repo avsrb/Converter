28092c85e06d6f0a7be17a5ee1d47472d6347751
Artem <Serebryakov>
2022-06-01 20:59:19 +0300

    first commit

--- Converter/ContentView.swift
+++ Converter/ContentView.swift
@@ -8,9 +8,42 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var inputNumber = 10.0
+    @State private var inputUnit = "meters"
+    @State private var outputUnit = "kilometers"
+    
+    let utils = ["meters", "kilometers", "feet", "yards", "miles"]
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        NavigationView {
+            Form {
+                Section {
+                    TextField("Amount", value: $inputNumber, format: .number)
+                        .keyboardType(.decimalPad)
+                } header: {
+                    Text("Amount to convert")
+                }
+                
+                Section {
+                    Picker("Convert from", selection: $inputUnit) {
+                        ForEach(utils, id: \.self) {
+                            Text($0)
+                        }
+                    }
+                }
+                Section {
+                    Picker("Convert from", selection: $outputUnit) {
+                        ForEach(utils, id: \.self) {
+                            Text($0)
+                        }
+                    }
+                }
+                
+                Text("Result")
+            }
+            .navigationTitle("Converter")
+        }
+       
     }
 }
 
