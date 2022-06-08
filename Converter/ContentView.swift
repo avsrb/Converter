ddacfc90dcd051f2e9327cb92d103b92d87ffc51
Artem <Serebryakov>
2022-06-08 21:22:55 +0300

    1.0

--- Converter/ContentView.swift
+++ Converter/ContentView.swift
@@ -10,7 +10,40 @@ import SwiftUI
 struct ContentView: View {
     @State private var inputNumber = 10.0
     @State private var inputUnit = "meters"
-    @State private var outputUnit = "kilometers"
+    @State private var outputUnit = "meters"
+    
+    private var outputNumber: Double {
+        var result: Double
+        var meters: Double
+    
+        // Convertion to meters. We storage value in meters
+        switch outputUnit {
+        case "kilometers":
+            meters = inputNumber * 0.001
+        case "miles":
+            meters = inputNumber * 0.000621371
+        case "feet":
+            meters = inputNumber * 3.28084
+        case "yards":
+            meters = inputNumber * 1.0944
+        default:
+            meters = inputNumber * 1.0
+        }
+        
+        switch inputUnit {
+        case "kilometers":
+            result = meters * 1000.0
+        case "miles":
+            result = meters * 1609.34
+        case "feet":
+            result = meters * 0.3048
+        case "yards":
+            result = meters * 0.9144
+        default:
+            result = meters * 1.0
+        }
+        return result
+    }
     
     let utils = ["meters", "kilometers", "feet", "yards", "miles"]
     
@@ -25,7 +58,7 @@ struct ContentView: View {
                 }
                 
                 Section {
-                    Picker("Convert from", selection: $inputUnit) {
+                    Picker("Convert to", selection: $inputUnit) {
                         ForEach(utils, id: \.self) {
                             Text($0)
                         }
@@ -39,7 +72,9 @@ struct ContentView: View {
                     }
                 }
                 
-                Text("Result")
+                Section{
+                    Text(outputNumber.formatted())
+                }
             }
             .navigationTitle("Converter")
         }
